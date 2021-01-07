e46f3540a1eaea1c85f31bd95357349be4027c5b
YevheniiLytvynhuk <59790323+YevheniiLytvynhuk@users.noreply.github.com>
2021-01-07 17:32:59 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,16 @@
+
+
+public class Car {
+	Car (String id,String brand,String model,String year_of_issue,String price,String registration_number){
+		public String id = id;
+		String Brand = brand;
+		String Model = model;
+		year_of_issue=year_of_issue;
+		String Price = price;
+		registration_number=registration_number;
+		
+		Car[] cars = new Car[1];
+		cars [0] = new Car(Id="1212", brand="audi", model="TT", year_of_issue="1992", Price="12000", registration_number="AA1212AA");
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	public String toString () {
+		return Id+"\n"+brand+"\n"+model+"\n"+year_of_issue+"\n"+price+"\n"+registration_number+"\n"
+
+			    
+	}
+	}
